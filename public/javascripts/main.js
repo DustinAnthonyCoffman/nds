42b40f3baf5700b5cb8bf5085b787acdab4c448a
Dustin Coffman <dustincoffman@Dustins-MBP.attlocal.net>
2020-04-18 20:24:01 -0500

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){
+    AOS.init();
+    $(".dropdown, .btn-group").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});  
+
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
---
+++ routes/contact.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const contactCtrl = require('../controllers/contact');
+//if the server request made it to here, then just specify the rest of the url, 
+//tickets/ is already part of the path because of its url origin ref="/tickets/new">
+
+router.get('/', contactCtrl.index)
+
+
+
+module.exports = router;
